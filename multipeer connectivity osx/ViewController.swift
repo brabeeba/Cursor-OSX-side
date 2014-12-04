43a3e0b582dc6d33c25a1a4e65282fade4487b4e
brabeeba <brabeeba@gmail.com>
2014-12-03 20:01:47 -0500

    Initial Commit

---
+++ multipeer connectivity osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  multipeer connectivity osx
+//
+//  Created by Brabeeba Wang on 12/3/14.
+//  Copyright (c) 2014 Brabeeba Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
